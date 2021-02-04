78b1a7ee7ddcd559aa02ca5d85bb7a9ec326ddbf
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2021-02-04 20:08:34 +0100

    update

--- pass.py
+++ pass.py
@@ -1,46 +1,49 @@
 import os,sys, random
 
 def add(args):
-	print('Adding')
-	with open('passwords.txt', 'a') as passfile:
-		info  =  '   '.join(args)
-		passfile.write(info)
-		print(f"{info} added!")
+        print('Adding')
+        with open('passwords.txt', 'a') as passfile:
+                info  =  '   '.join(args)
+                passfile.write(info)
+                print(f"{info} added!")
 
 def gen(args):
-	chars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890[];:.,<>?/|~`!@#$%^&*()_-+=")
-	password = ""
-	for _ in range(random.choice(range(15, 21))):
-		password += random.choice(chars)
-	print(password)
-	args.append(password)
-	add(args)	
+        chars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890[];:.,<>?/|~`!@#$%^&*()_-+=")
+        password = ""
+        for _ in range(random.choice(range(15, 21))):
+                password += random.choice(chars)
+        print(password)
+        args.append(password)
+        add(args)       
 
 
 def find(args):
-	with open('passwords.txt', 'r') as passfile:
-		for line in passfile.readlines():
-			info =  line.split('   ')
-			for field in info:
-				if args[0] in field:
-					print('   '.join(info).strip())			
+        with open('passwords.txt', 'r') as passfile:
+                for line in passfile.readlines():
+                        info =  line.split('   ')
+                        for field in info:
+                                if args[0] in field:
+                                        print('   '.join(info).strip())                 
 def remove(args):
-	lines = []
-	with open('passwords.txt', 'r') as passfile:
-		lines = passfile.readlines()
+        lines = []
+        removeIdx  = []
+        with open('passwords.txt', 'r') as passfile:
+                lines = passfile.readlines()
 
-	with open('passwords.txt', 'w') as passfile:
-		for line in lines:
-			info =  line.split('   ')
-			for field in info:
-				if args[0] in field:
-					confirm = input(f"Do you want to delete field {'   '.join(info).strip()}  (y/n)  ? ")
-					if confirm == 'y':
-						lines.remove(line)
-						print(f"{'   '.join(info).strip()} deleted")			
-
-		for line in lines:
-			passfile.write(line)
+        with open('passwords.txt', 'w') as passfile:
+                for line in lines:
+                        info =  line.split('   ')
+                        for field in info:
+                                if args[0] in field:
+                                        confirm = input(f"Do you want to delete field {'   '.join(info).strip()}  (y/n)  ? ")
+                                        if confirm == 'y':
+                                                removeIdx.append(line)  
+                                                print(f"{'   '.join(info).strip()} deleted")                    
+                                        
+                    
+                for line in lines:
+                    if line not in removeIdx:
+                        passfile.write(line)
 
 
 def commit(args):
@@ -66,7 +69,7 @@ def main():
     except IndexError:
             print('Not enough args')
     except Exception as err:
-	    print(f"An exception occured {err}")
+            print(f"An exception occured {err}")
 
 
 if __name__ == "__main__":
